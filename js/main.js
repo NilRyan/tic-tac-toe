8d80c664ccc5fd21d4ccd78eae54b600ce31073a
Neil Ryan <neilryanlipaod@gmail.com>
2021-01-26 17:47:14 +0800

    reset and game result display added

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 const boardWrapper = document.querySelector('.board-wrapper')
+const scorePanel = document.querySelector('.score-panel');
 
 // create the board
-for( let i = 0; i < 9 ; i++){
+for (let i = 0; i < 9; i++) {
   const board = document.createElement('div');
-  board.classList.add('panel');
+  board.classList.add('panel', i);
   board.setAttribute('data-index', i);
   boardWrapper.appendChild(board);
 }
@@ -12,15 +13,15 @@ for( let i = 0; i < 9 ; i++){
 const gameBoard = (() => {
   const boardArray = Array(9);
   const x = (index) => {
-    if(boardArray[index] != 'X' && boardArray[index] != 'O' ){
-        boardArray[index] = 'X';
-        boardWrapper.children[index].textContent = boardArray[index];
+    if (boardArray[index] != 'X' && boardArray[index] != 'O') {
+      boardArray[index] = 'X';
+      boardWrapper.children[index].textContent = boardArray[index];
     }
   }
   const o = (index) => {
-    if(boardArray[index] != 'X' && boardArray[index] != 'O' ){
-        boardArray[index] = 'O';
-        boardWrapper.children[index].textContent = boardArray[index];
+    if (boardArray[index] != 'X' && boardArray[index] != 'O') {
+      boardArray[index] = 'O';
+      boardWrapper.children[index].textContent = boardArray[index];
     }
   }
   //clears the board
@@ -30,35 +31,72 @@ const gameBoard = (() => {
       boardWrapper.children[index].textContent = '';
     })
   }
-  //check board if game is won
+  //check board if game is won and clears the board
   const winCheck = (marker) => {
     //marker is X or O
-    const vertical = [0,3,6].map(i=>{return[i,i+1,i+2]});
-    const horizontal = [0,1,2].map(i=>{return[i,i+3,i+6]});
-    const diagonal = [[2,4,6],[0, 4, 8]];
-  
-    const winArray = [].concat(vertical).concat(horizontal).concat(diagonal);
-    
-    let result = winArray.some(indices => { 
+    const vertical = [0, 3, 6].map(i => {
+      return [i, i + 1, i + 2]
+    });
+    const horizontal = [0, 1, 2].map(i => {
+      return [i, i + 3, i + 6]
+    });
+    const diagonal = [
+      [2, 4, 6],
+      [0, 4, 8]
+    ];
 
-    return boardArray[indices[0]] === marker && boardArray[indices[1]] === marker && boardArray[indices[2]] === marker})
-    if( result === true ){
-      alert('Player ' + marker + ' won');
-      gameBoard.clear();
+    const winArray = [].concat(vertical).concat(horizontal).concat(diagonal);
+
+    let result = winArray.some(indices => {
+
+      return boardArray[indices[0]] === marker && boardArray[indices[1]] === marker && boardArray[indices[2]] === marker
+    })
+    if (result === true) {
+      return marker;
+
+    } else if (boardArray.filter(x => x === 'X' || x === 'O').length === 9) {
+      return 'TIE';
     }
   }
-  
+  //display game result in board
+  const displayResult = (result) => {
+    if (result === 'X') {
+      boardWrapper.children[0].textContent = 'Y';
+      boardWrapper.children[1].textContent = 'O';
+      boardWrapper.children[2].textContent = 'U';
+      boardWrapper.children[3].textContent = 'W';
+      boardWrapper.children[4].textContent = 'I';
+      boardWrapper.children[5].textContent = 'N';
+    } else if (result === 'TIE') {
+      boardWrapper.children[3].textContent = 'T';
+      boardWrapper.children[4].textContent = 'I';
+      boardWrapper.children[5].textContent = 'E';
+    } else if (result === 'O') {
+      boardWrapper.children[0].textContent = 'T';
+      boardWrapper.children[1].textContent = 'R';
+      boardWrapper.children[2].textContent = 'Y';
+      boardWrapper.children[6].textContent = 'A';
+      boardWrapper.children[7].textContent = 'G';
+      boardWrapper.children[8].textContent = 'N';
+    }
+  };
   //computer generates random
   const computer = () => {
     let index = Math.floor(Math.random() * 9);
-    while (boardArray[index] === 'X' || boardArray[index] === 'O' && boardArray.includes(undefined) ){
+    while (boardArray[index] === 'X' || boardArray[index] === 'O' && boardArray.includes(undefined)) {
       index = Math.floor(Math.random() * 9);
     }
     console.log(index);
     return index;
   }
   return {
-    boardArray, x, o, clear, computer, winCheck
+    boardArray,
+    x,
+    o,
+    clear,
+    computer,
+    winCheck,
+    displayResult
   };
 
 })();
@@ -69,28 +107,22 @@ boardWrapper.addEventListener('click', e => {
   const index = e.target.dataset.index;
   console.log(index);
   gameBoard.x(index);
-  setTimeout(function (){gameBoard.o(gameBoard.computer())}, 250);
   setTimeout(function () {
-  gameBoard.winCheck('X');
-  gameBoard.winCheck('O');  },
-  300)
+    gameBoard.o(gameBoard.computer())
+  }, 200);
+  setTimeout(function () {
+    gameBoard.displayResult(gameBoard.winCheck('X'));
+    gameBoard.displayResult(gameBoard.winCheck('O'))
+  }, 600);
+
 })
 
 
-
-
-//clearing the board
+//clearing the board using events
 window.addEventListener('click', e => {
   e.stopPropagation;
-  
-  if (e.target.classList.contains('top-bar')){
+
+  if (e.target.classList.contains('reset')) {
     gameBoard.clear();
   }
 })
-
-
-
-// const gameBoard = () => {
-//   const boardArray = Array.from(boardWrapper.children.value);
-//   console.table(boardArray);
-// }
